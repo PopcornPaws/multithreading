243313c92d24f3a5dc2db20037504dd814091fc3
Mark Melczer <melczer7@gmail.com>
2022-06-07 18:20:12 +0200

    figure out how rayon works

--- src/lib.rs
+++ src/lib.rs
@@ -40,8 +40,9 @@ impl Text {
         Self { inner }
     }
 
-    pub fn process(self, concurrency: usize, pool: &pool::WorkerPool) {
+    pub fn process(self, concurrency: usize, input: String, pool: &pool::WorkerPool) -> Result<ProcessedText, JsValue> {
         let mut map = CharMap::new();
+        let n_chunks = (input.len() / concurrency).max(1);
 
         let thread_pool = rayon::ThreadPoolBuilder::new()
             .num_threads(concurrency)
@@ -50,14 +51,33 @@ impl Text {
             .unwrap();
 
         let (tx, rx) = oneshot::channel();
-        pool.run(move || thread_pool.install(|| {}));
-        drop(tx.send(todo!()));
-        todo!();
+        pool.run(move || {
+            thread_pool.install(|| {
+                input.chars().par_chunks(n_chunks)
+                    .for_each(|chunk| {
+                        let string = chunk.join("");
+                        for c in input.chars().filter(|c| c.is_alphabetic()) {
+                            *map.entry(c.to_ascii_lowercase()).or_default() += 1;
+                        }
+                    })
+            });
+            drop(tx.send(map));
+        })?;
+
+        let done = async move {
+            match rx.await {
+                Ok(data) => Ok(data.into()),
+                Err(_) => Err(JsValue::undefined()),
+            }
+        };
+
+        Ok(ProcessedText {
+            promise: wasm_bindgen_futures::future_to_promise(done),
+        })
     }
 }
 
 #[wasm_bindgen]
 pub struct ProcessedText {
     promise: Promise,
-    map: HashMap<char, usize>,
 }
